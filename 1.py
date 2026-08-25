91f3dd7044df0ebc908e36731d9a01d2c0ce0811
Bhaskar <ombhaskar2252005@gmail.com>
2026-08-25 12:00:06 +0530

    add files

---
+++ 1.py
@@ -0,0 +1,12 @@
+#write a python program to print your name
+#write a python program to add, subtract and multiply 2 number
+print("My name is Bhaskar")
+a = 10
+b = 5
+print("Addition: " , a+b)
+print("Subtract: ", a-b)
+print("Multiply: " , a*b)
+g = int(input("Enter a number: "))
+j = int(input("Enter 2nd number: "))
+subtract = g - j
+print(subtract)
--- input.py
+++ input.py
@@ -1,15 +1,15 @@
-a = input("Enter first number: ")
-b = input("Enter second number: ")
-sum = int(a) + int(b)
+a = int(input("Enter first Number: "))
+b = int (input("Enter a second Number: "))
+sum = a + b
 average = sum/2
-print( average)
-c = input("Enter the number: ")
-square = int(c) * int(c)
-print("the square is: ", square)
-#calculator
-add = print("Addition: ", sum)
-sub = print(int(a) - int(b))
-mul = print(int(a) * int(b))
-div = print(int(a) / int(b))
-remainder = print( int(a) % int(b))
-floordiv = print(int(a) // int(b))
+print(average)
+c = int(input("Enter the number: "))
+square = c*c
+print("The square is : " , square)
+d = float(input("Enter the decimal: "))
+divide = d/2
+print(divide)
+#round of 
+marks = float(input("enter the marks: "))
+roundof = round(marks, 2)
+print(roundof)
