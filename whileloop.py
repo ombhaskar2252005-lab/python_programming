15ce875cf6501f51f78652246bdaf284e95d3964
Bhaskar <ombhaskar2252005@gmail.com>
2026-09-04 02:02:34 +0530

    add files

--- whileloop.py
+++ whileloop.py
@@ -8,13 +8,64 @@
 # while n>0:
 #     print(n)
 #     n = n-1
-#maximum number
-n= int(input("Enter how many numbers: "))
-i =1
-maximum = 0
-while i<=n:
-    num = int(input("Enter a number: "))
-    if num > maximum:
-        maximum = num
-        i = i +1
-        print("Maximum number is: ", maximum)
+
+#count 1 to 10
+# i =1
+# while i<=10:
+#     print(i)
+#     i = i+1
+
+#even number count
+# n = int(input("Enter a number: "))
+# i = 1
+# count =0
+# while i<=n:
+#     if (i % 2 == 0):
+#         count = count +1
+#     i = i+1
+# print(count)
+
+#table of n
+# n = int(input("Enter a number: "))
+# i = 1
+# while i <= 10:
+#     print(n*i)
+#     i = i + 1
+
+
+#sum 1 to n
+# n = int(input("Enter a number: "))
+# i =1 
+# sum =0
+# while i <=n:
+#     sum = sum + i
+#     i = i+1
+# print(sum)
+
+#sum of odd number
+# n = int(input("Enter a number: "))
+# i =1
+# sum = 0
+# while i<=n:
+#     if(i%2 != 0):
+#         sum = sum +i
+#     i = i+1
+# print(sum)
+
+
+#reverse number
+# n = int(input("Enter a number: "))
+# rev =0
+# while n>0:
+#     temp = n%10 #remainder deta ha yah last digit deta ha
+#     rev = rev*10 + temp
+#     n = n//10 #decimal number hata deta ha or last digit ko hata deta ha
+# print(rev)
+
+#count the number of digits
+n = int(input("enter number: "))
+count = 0
+while n>0:
+    n = n//10
+    count = count + 1
+print(count)
